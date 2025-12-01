0ff00abef6b98b0dd53038a1cd59500145cfa74c
PhantomBoss05 <144142306+PhantomBoss05@users.noreply.github.com>
2025-12-01 10:44:27 +0100

    updated gitignore

---
+++ utils.py
@@ -0,0 +1,9 @@
+import os
+
+class Reader:
+    def __init__(self, file):
+        self.file = file
+    def read_txt_to_str(self) -> str:
+        file = os.path.join(os.path.dirname(os.path.realpath(__file__)), self.file)
+        with open(file, 'r', encoding='utf-8') as f:
+            return f.read()
